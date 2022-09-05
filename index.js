4be2f38852bb1ea66db32740408f0711c850f7c2
unknown <vintoxen@gmail.com>
2022-09-05 13:28:56 +0300

    this 2

--- index.js
+++ index.js
@@ -30,3 +30,18 @@
 // };
 //
 // ladder.up().up().down().showStep().down().showStep();
+
+let calculator = {
+    sum() {
+        return this.a + this.b;
+    },
+
+    mul() {
+        return this.a * this.b;
+    },
+
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+};
