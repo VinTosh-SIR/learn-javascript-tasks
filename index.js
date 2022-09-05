eb5281532f1343aca5b632caea83efee1513f952
unknown <vintoxen@gmail.com>
2022-09-05 13:25:53 +0300

    this

--- index.js
+++ index.js
@@ -1,91 +1,32 @@
-// 1 Task
-// Напишите код, выполнив задание из каждого пункта отдельной строкой:
-//     Создайте пустой объект user.
-//     Добавьте свойство name со значением John.
-//     Добавьте свойство surname со значением Smith.
-//     Измените значение свойства name на Pete.
-//     Удалите свойство name из объекта.
-let user = {}
-//литеральный способ
-// let user = new Object(); // конструктор обьектов
-user.name = "John";
-user.surname = "Smith";
-user.name = "Pete";
-delete user.name;
-
-//Task 2
-//     Напишите функцию isEmpty(obj), которая возвращает true, если у объекта нет свойств, иначе false.
-//     Должно работать так:
-// let schedule = {};
-//
-// alert( isEmpty(schedule) ); // true
-//
-// schedule["8:30"] = "get up";
-//
-// alert( isEmpty(schedule) ); // false
-
-function isEmpty(obj) {
-    for (let key in obj) {
-        return false;
-    }
-    return true;
-}
-
-let schedule = {};
-
-alert( isEmpty(schedule) ); // true
-
-schedule["8:30"] = "get up";
-
-alert( isEmpty(schedule) ); // false
-
-//Task 4
-//У нас есть объект, в котором хранятся зарплаты нашей команды:
-// let salaries = {
-//     John: 100,
-//     Ann: 160,
-//     Pete: 130
-// }
-// Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. Должно получиться 390.
-//
-// Если объект salaries пуст, то результат должен быть 0.
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-}
-let sum = 0;
-for (let key in salaries){
-    sum += salaries[key];
-}
-console.log(sum)
-
-
-//Task 5
-// Создайте функцию multiplyNumeric(obj), которая умножает все числовые свойства объекта obj на 2.
-//
-// Например:
-//
-// до вызова функции
-//     let menu = {
-//         width: 200,
-//         height: 300,
-//         title: "My menu"
+// function makeUser() {
+//     return {
+//         name: "John",
+//         age: 20,
+//         ref(){
+//             return this;
+//             }
 //     };
+// }
 //
-// multiplyNumeric(menu);
+// let user = makeUser();
 //
-// после вызова функции
-// menu = {
-//     width: 400,
-//     height: 600,
-//     title: "My menu"
-// };
+// console.log( user.ref().age);
 
-function multiplyNumeric(obj) {
-    for (let key in obj) {
-        if (typeof obj[key] == 'number') {
-            obj[key] *= 2;
-        }
-    }
-}
+
+// let ladder = {
+//     step: 0,
+//     up() {
+//         this.step++;
+//         return this;
+//     },
+//     down() {
+//         this.step--;
+//         return this;
+//     },
+//     showStep() {
+//         alert( this.step );
+//         return this;
+//     }
+// };
+//
+// ladder.up().up().down().showStep().down().showStep();
